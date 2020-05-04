38cd9fb61e2a377c4fbce47570aec049566bdf89
HGJ <huguangjing0411@geektip.cc>
2020-05-05 02:50:32 +0800

    v0.4

--- a.py
+++ a.py
@@ -6,6 +6,10 @@ class A:
     def __init__(self):
         self.tree=set()
         self.call={'测试':self.test,'挂树':self.add,'查树':self.get,'砍树':self.clear,'下树':self.erase}
+    def count(self):
+        return '树上共%d人'%len(self.tree)
+    def check(self):
+        return self.param['sender']['role']=='member'and self.sender!='hgjazhgj'#权限狗的快乐
     def test(self):
         return '测试返回'
     def add(self):
@@ -13,8 +17,8 @@ class A:
             if self.sender in self.tree:
                 return '你已经在树上了'
             self.tree.add(self.sender)
-            return '已挂树,当前树上%d人'%len(self.tree)
-        if self.param['sender']['role']=='member'and self.sender!='hgjazhgj':
+            return '已挂树,'+self.count()
+        if self.check():
             return '没有权限'
         ans=''
         for i in self.cmd[1:]:
@@ -23,11 +27,11 @@ class A:
                 continue
             self.tree.add(i)
             ans+=i+' 添加成功\n'
-        return ans+'当前树上%d人'%len(self.tree)
+        return ans+self.count()
     def get(self):
-        return '树上共%d人\n'%len(self.tree)+'\n'.join(('%2d. %s'%(i,j)for i,j in enumerate(self.tree)))
+        return self.count()+'\n'.join((str(i)+'.'+(' 'if i<10 else'')+j for i,j in enumerate(self.tree)))
     def clear(self):
-        if self.param['sender']['role']=='member'and self.sender!='hgjazhgj':
+        if self.check():
             return '没有权限'
         self.tree.clear()
         return '砍树成功'
@@ -37,7 +41,7 @@ class A:
                 self.tree.remove(self.sender)
                 return '下树成功'
             return '你不在树上'
-        if self.param['sender']['role']=='member'and self.sender!='hgjazhgj':
+        if self.check():
             return '没有权限'
         ans=''
         for i in self.cmd[1:]:
@@ -46,11 +50,12 @@ class A:
                 ans+=i+' 移除成功\n'
                 continue
             ans+=i+' 移除失败\n'
-        return ans+'当前树上%d人'%len(self.tree)
+        return ans+self.count()
     def __call__(self,param):
         try:
             if not param['message'].startswith('/'):
                 return '__'
+            print(param)
             self.param=param
             try:
                 self.cmd=[i for i in param['message'].split(' ') if i]
@@ -68,7 +73,6 @@ app=Flask(__name__)
 def api():
     try:
         js=request.get_json()
-        print(js)
         t=a(js)
         if t!='__':
             post('http://localhost:5700/send_group_msg',data={'group_id':js['group_id'],'message':t})
@@ -78,5 +82,4 @@ def api():
     except:
         print_exc()
         return b'Fuck'
-
 app.run(host='localhost',port=8000,debug=False)
