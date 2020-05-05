8c02a22478c8f93109179b31753321fb9b4fcf01
HGJ <huguangjing0411@geektip.cc>
2020-05-05 09:58:31 +0800

    v0.5

--- a.py
+++ a.py
@@ -1,25 +1,24 @@
-﻿from flask import Flask,request,jsonify
+﻿from flask import Flask,request
 from requests import post
 from traceback import print_exc
-
 class A:
     def __init__(self):
         self.tree=set()
         self.call={'测试':self.test,'挂树':self.add,'下树':self.erase,'查树':self.get,'砍树':self.clear}
     def count(self):
-        return '树上共%d人'%len(self.tree)
+        return'树上共%d人'%len(self.tree)
     def check(self):
         return self.param['sender']['role']=='member'and self.sender!='hgjazhgj'#权限狗的快乐
     def test(self):
-        return '测试返回'
+        return'测试返回 '+str(self.cmd)
     def add(self):
         if len(self.cmd)==1:
             if self.sender in self.tree:
-                return '你已经在树上了'
+                return'你已经在树上了'
             self.tree.add(self.sender)
-            return '已挂树,'+self.count()
+            return'已挂树,'+self.count()
         if self.check():
-            return '没有权限'
+            return'没有权限'
         ans=''
         for i in self.cmd[1:]:
             if i in self.tree:
@@ -29,20 +28,20 @@ class A:
             ans+=i+' 添加成功\n'
         return ans+self.count()
     def get(self):
-        return self.count()+'\n'.join((str(i)+'.'+(' 'if i<10 else'')+j for i,j in enumerate(self.tree)))
+        return self.count()+'\n'+'\n'.join((str(i+1)+'.'+(' 'if i<9else'')+j for i,j in enumerate(self.tree)))
     def clear(self):
         if self.check():
-            return '没有权限'
+            return'没有权限'
         self.tree.clear()
-        return '砍树成功'
+        return'砍树成功'
     def erase(self):
         if len(self.cmd)==1:
             if self.sender in self.tree:
                 self.tree.remove(self.sender)
-                return '下树成功'
-            return '你不在树上'
+                return'下树成功'
+            return'你不在树上'
         if self.check():
-            return '没有权限'
+            return'没有权限'
         ans=''
         for i in self.cmd[1:]:
             if i in self.tree:
@@ -62,10 +61,10 @@ class A:
                 self.sender=param['sender']['card']if param['sender']['card']else param['sender']['nickname']
                 return self.call[self.cmd[0][1:]]()
             except KeyError:
-                return '错误'
+                return'错误'
         except:
             print_exc()
-            return '__'
+            return'__'
 a=A()
 
 app=Flask(__name__)
