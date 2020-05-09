c7f4abaf6350c3e289d4d1ff52b11ad048eac8e3
HGJ <huguangjing0411@geektip.cc>
2020-05-09 15:33:50 +0800

    v1.4

--- main.py
+++ main.py
@@ -10,37 +10,53 @@ class A:
         self.treeData=set()
         self.simulData={}
         self.debug=None
-        self.call={'测试':self.test,'exec':self.exec,'eval':self.eval,
+        self.locked=False
+        self.call={'测试':self.test,'exec':self.exec,'eval':self.eval,'lock':self.lock,'unlock':self.unlock,
                    '挂树':self.addTree,'下树':self.eraseTree,'查树':self.getTree,'砍树':self.clearTree,
                    '模拟出刀':self.addSimul,'查看模拟刀':self.getSimul,'清空模拟刀':self.clearSimul}
+    def check(self,x=0):#白名单1检查锁2群主管理员可用4成员可用
+        def wrapper(func):
+            def inner(*args,**kwargs):
+                if self.param['sender']['user_id']==979449732:#是我就直接放行,不管锁没锁
+                    return func(*args,**kwargs)
+                if x&1 and self.locked:
+                    return'禁止访问'
+                return func(*args,**kwargs)if x&2 and self.param['sender']['role']!='member'or x&4and self.param['sender']['role']=='member'else'没有权限'
+            return inner
+        return wrapper
+    @self.check()
+    def lock(self):
+        self.locked=True
+        return'完成'
+    @self.check()
+    def unlock(self):
+        self.locked=False
+        return'完成'
+    @self.check()
     def exec(self):
-        if self.param['sender']['user_id']!=979449732:
-            return'__'
         try:
             exec(self.param['message'][6:])
-            return'Done'
+            return'完成'
         except BaseException as e:
             return'错误:'+str(e)
+    @self.check()
     def eval(self):
-        if self.param['sender']['user_id']!=979449732:
-            return'__'
         try:
             return str(eval(self.param['message'][6:]))
         except BaseException as e:
             return'错误:'+str(e)
     def countTree(self):
         return'树上共%d人'%len(self.treeData)
-    def check(self):
-        return self.param['sender']['role']=='member'and self.param['sender']['user_id']!=979449732#权限狗的快乐
     def test(self):
         return'测试返回 '+str(self.cmd)
+    @self.check(7)
     def addTree(self):
         if len(self.cmd)==1:
             if self.sender in self.treeData:
                 return'你已经在树上了'
             self.treeData.add(self.sender)
             return'已挂树,'+self.countTree()
-        if self.check():
+        if self.param['sender']['role']=='member':
             return'没有权限'
         ans=''
         for i in self.cmd[1:]:
@@ -52,18 +68,18 @@ class A:
         return ans+self.countTree()
     def getTree(self):
         return self.countTree()+'\n'+'\n'.join((str(i+1)+'.'+('  'if i<9else'')+j for i,j in enumerate(self.treeData)))
+    @self.check(3)
     def clearTree(self):
-        if self.check():
-            return'没有权限'
         self.treeData.clear()
         return'砍树成功'
+    @self.check(7)
     def eraseTree(self):
         if len(self.cmd)==1:
             if self.sender in self.treeData:
                 self.treeData.remove(self.sender)
                 return'下树成功'
             return'你不在树上'
-        if self.check():
+        if self.param['sender']['role']=='member':
             return'没有权限'
         ans=''
         for i in self.cmd[1:]:
@@ -75,6 +91,7 @@ class A:
         return ans+self.countTree()
     def countSimul(self):
         return'已有%d模拟刀'%len(self.simulData)
+    @self.check(7)
     def addSimul(self):
         if len(self.cmd)!=4:
             return'错误:需要正好3个参数'
@@ -84,10 +101,9 @@ class A:
         except BaseException as e:
             return str(e)
     def getSimul(self):
-        return self.countSimul()+'\n排名 用户 刀序 伤害\n'+'\n'.join(('%d  %s  %d  %d'%(i,*j)for i,j in enumerate(sorted(((i,j,self.simulData[i][j])for i in self.simulData for j in range(1,4)),key=lambda x:-x[2]))))
+        return self.countSimul()+'\n排名_用户_刀序_伤害\n'+'\n'.join(('%d_%s_%d_%d'%(i,*j)for i,j in enumerate(sorted(((i,j,self.simulData[i][j])for i in self.simulData for j in range(1,4)),key=lambda x:-x[2]))))
+    @self.check(3)
     def clearSimul(self):
-        if self.check():
-            return'没有权限'
         self.simulData.clear()
         return'清除成功'
     def __call__(self,param):
